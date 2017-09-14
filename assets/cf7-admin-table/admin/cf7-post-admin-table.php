509098e8b755300af553a2fb34044bfb673486c9
Aurovrata V <vrata@syllogic.in>
2017-09-15 02:06:27 +0530

    cf7-form shortcode

--- assets/cf7-admin-table/admin/cf7-post-admin-table.php
+++ assets/cf7-admin-table/admin/cf7-post-admin-table.php
@@ -419,7 +419,7 @@ if(!class_exists('Cf7_WP_Post_Table')){
      * @return     string    $p2     .
     **/
     public function shortcode( $atts ) {
-      $a = shortcode_atts( array(
+      $a = array_merge( array(
           'cf7key' => '',
       ), $atts );
       if(empty($a['cf7key'])){
