3acb0930d00853d2b3f485b64baedbb692197372
aurovrata <vrata.venet@gmail.com>
2021-10-01 19:02:37 +0530

    pointers

---
+++ admin/partials/pointers/cf7sg-pointer-tag-dynamic-checkbox.php
@@ -0,0 +1,8 @@
+<?php
+//dynamic dropdown tag.
+ ?>
+<h3 class="cf7sg-pointer"><?=__('Dynamic checkbox field', 'cf7-grid-layout')?></h3>
+<div>
+  <p><?=__('This tag allows you to build a dynamic list of checkbox fields of taxonomy terms, post titles, or programmatically created values using the filter.  The field also allows for custom contructs such as image grids.','cf7-grid-layout')?></p>
+  
+</div>
