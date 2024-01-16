e3135d80a2ec641183e968131d627cad08d0714b
aurovrata <vrata.venet@gmail.com>
2024-01-16 14:29:23 +0530

    v4.15.7
    scan textarea tag

--- admin/js/cf7-grid-layout-admin.js
+++ admin/js/cf7-grid-layout-admin.js
@@ -1034,7 +1034,7 @@
           break;
         case 'textarea'==tag:
           if( match[0].search(/\s[0-9]{0,3}x[0-9]{1,3}\s?/ig) <0){
-            let cf7sc = match[0].replace(match[3],match[3]+' x5'); //textarea prefill.
+            let cf7sc = match[0].replace(field,field + ' x5'); //textarea prefill.
             cf7sc = search.replace(match[0], cf7sc);
             $this.val(cf7sc);
           }
